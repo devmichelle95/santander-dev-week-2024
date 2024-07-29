8b60be90126a00fc62c65127e7987cbae267129d
michelledev95 <28oliveira.michelle@gmail.com>
2024-07-29 13:47:09 -0300

    first commit

---
+++ santander-dev-week-2024/src/main/java/me/dio/SantanderDevWeek2024Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2024Application.class, args);
+	}
+
+}
